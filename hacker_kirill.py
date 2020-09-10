6659d4d8c62cdbef04105234d637bad903cf5ee0
hEllkiller113 <70215309+hEllkiller113@users.noreply.github.com>
2020-09-10 21:10:55 +0800

    Add files via upload

---
+++ hacker_kirill.py
@@ -0,0 +1,9 @@
+index = str(input("Введите 7: "))
+alysm = str(input("Кирилл: "))
+
+if index == "7":
+    pass
+if alysm == "Кирилл":
+    print("хакер", alysm, index, "строчек кода")
+else:
+    print("иблан?")
